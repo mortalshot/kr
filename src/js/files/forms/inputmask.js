e75d96b6e3f01bdb5c56c66a96689ea0c5880071
mortal <seregagl1996@gmail.com>
2022-04-13 11:02:42 +0300

    profile

--- src/js/files/forms/inputmask.js
+++ src/js/files/forms/inputmask.js
@@ -7,7 +7,7 @@ import { flsModules } from "../modules.js";
 // Подключение модуля
 import "inputmask/dist/inputmask.min.js";
 
-const inputMasks = document.querySelectorAll('input');
-if (inputMasks.length) {
-	flsModules.inputmask = Inputmask().mask(inputMasks);
+const phoneMask = document.querySelectorAll('.form__input_phone');
+if (phoneMask.length) {
+	flsModules.inputmask = Inputmask('+7 999 999 99 99').mask(phoneMask);
 }
