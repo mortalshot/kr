67704102304983038a2eb7d36e54097bfa2031ce
mortal <seregagl1996@gmail.com>
2022-04-19 11:13:59 +0300

    test

--- dist/js/app.js
+++ dist/js/app.js
@@ -8499,59 +8499,6 @@ PERFORMANCE OF THIS SOFTWARE.
     };
     const da = new DynamicAdapt("max");
     da.init();
-    const headerWrapper = document.querySelector(".header__wrapper");
-    const navOffset = headerWrapper.offsetTop;
-    const wrapper = document.querySelector(".wrapper");
-    const mainPage = document.querySelector("main.page");
-    window.addEventListener("scroll", (function() {
-        const scrollTop = window.scrollY;
-        if (scrollTop > navOffset) {
-            wrapper.classList.add("header-fixed");
-            const headerHeight = headerWrapper.clientHeight;
-            mainPage.style.paddingTop = headerHeight + "px";
-        } else if (scrollTop < navOffset) {
-            wrapper.classList.remove("header-fixed");
-            mainPage.style.paddingTop = 0;
-        }
-    }));
-    const sidebar = document.querySelector(".catalog__sidebar");
-    document.addEventListener("click", documentActions);
-    function documentActions(e) {
-        const targetElement = e.target;
-        if (targetElement.classList.contains("catalog__filter-btn") || targetElement.closest(".catalog__filter-btn")) {
-            e.preventDefault();
-            sidebar.classList.add("_active");
-            bodyLock();
-        }
-        if (targetElement.classList.contains("filter__close") || targetElement.closest(".filter__close")) {
-            e.preventDefault();
-            sidebar.classList.remove("_active");
-            bodyUnlock();
-        }
-    }
-    const attributeItems = document.querySelectorAll(".cart-item__number");
-    if (attributeItems.length > 0) document.addEventListener("selectCallback", (function(e) {
-        const currentSelect = e.detail.select;
-        if ("number" === currentSelect.getAttribute("name")) {
-            console.log(currentSelect.value);
-            if ("10+" == currentSelect.value) {
-                currentSelect.closest(".cart-item__input").querySelector(".form__input").style.display = "block";
-                currentSelect.closest(".select").style.display = "none";
-            }
-        }
-    }));
-    const filterItems = document.querySelector(".filter__items");
-    if (filterItems) {
-        let filterMmd3 = window.matchMedia("(min-width: 767.98px)");
-        function filterHandleMmd3Change(e) {
-            if (e.matches) {
-                const header = document.querySelector(".header");
-                filterItems.style.maxHeight = window.innerHeight - header.clientHeight - 100 + "px";
-            }
-        }
-        filterMmd3.addEventListener("change", filterHandleMmd3Change);
-        filterHandleMmd3Change(filterMmd3);
-    }
     window["FLS"] = true;
     isWebp();
     addLoadedClass();
